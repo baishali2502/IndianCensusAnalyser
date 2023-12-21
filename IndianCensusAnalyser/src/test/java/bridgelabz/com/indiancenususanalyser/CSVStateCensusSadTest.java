b9915a628796d63432498dabf9d1e51938189fa8
baishali2502 <baishali2502@gmail.com>
2023-12-22 01:56:51 +0530

    Added TC-1.4:Throw custom exception for incorrect delimiter

--- IndianCensusAnalyser/src/test/java/bridgelabz/com/indiancenususanalyser/CSVStateCensusSadTest.java
+++ IndianCensusAnalyser/src/test/java/bridgelabz/com/indiancenususanalyser/CSVStateCensusSadTest.java
@@ -75,4 +75,35 @@ public class CSVStateCensusSadTest {
         // Attempt to load census data from the correct CSV file with incorrect types
         csvStateCensus.loadCensusData(correctCsvFilePath);
     }
+    /**
+     * @desc
+     * This test case checks whether the CSVStateCensus class throws a custom exception
+     * (InvalidCensusDataException) when the file delimiter is incorrect in a correct state census CSV file.
+     * This is a sad test case to ensure that an exception is raised in case of an incorrect delimiter.
+     *
+     * @params
+     * The test uses the following parameters:
+     * - correctCsvFilePath: A string representing the path to the correct CSV file
+     *                       with an incorrect delimiter used for testing.
+     *
+     * @returns
+     * This test case does not return any value. It expects the InvalidCensusDataException
+     * to be thrown during the execution of the loadCensusData method. If the method throws
+     * the expected exception, the test passes; otherwise, it fails.
+     *
+     * @throws IOException               If an I/O error occurs during the test.
+     * @throws CsvException              If an error occurs while processing the CSV file.
+     * @throws InvalidCensusDataException If the CSV file has correct data but an incorrect delimiter.
+     */
+    @Test(expected = InvalidCensusDataException.class)
+    public void testLoadCensusDataWithIncorrectDelimiter() throws IOException, CsvException, InvalidCensusDataException {
+        // Specify a correct CSV file path with an incorrect delimiter for testing
+        String correctCsvFilePath = "C:\\Users\\KIIT\\OneDrive\\Desktop\\StateCensus3.csv";
+
+        // Create CSVStateCensus instance
+        CSVStateCensus csvStateCensus = new CSVStateCensus();
+
+        // Attempt to load census data from the correct CSV file with an incorrect delimiter
+        csvStateCensus.loadCensusData(correctCsvFilePath);
+    }
 }

f66f0fc78b0169ef604bac6bd416947d9f85f296
baishali2502 <baishali2502@gmail.com>
2023-12-22 01:37:54 +0530

    Added TC-1.3:Throw custom exception when the file is correct but type is incorrect

--- IndianCensusAnalyser/src/test/java/bridgelabz/com/indiancenususanalyser/CSVStateCensusSadTest.java
+++ IndianCensusAnalyser/src/test/java/bridgelabz/com/indiancenususanalyser/CSVStateCensusSadTest.java
@@ -44,4 +44,35 @@ public class CSVStateCensusSadTest {
         // Attempt to load census data from the incorrect CSV file
         csvStateCensus.loadCensusData(incorrectCsvFilePath);
     }
+    /**
+     * @desc
+     * This test case checks whether the CSVStateCensus class throws a custom exception
+     * (InvalidCensusDataException) when the type is incorrect in the correct state census CSV file.
+     * This is a sad test case to ensure that an exception is raised in case of incorrect types.
+     *
+     * @params
+     * The test uses the following parameters:
+     * - correctCsvFilePath: A string representing the path to the correct CSV file
+     *                       with an incorrect type used for testing.
+     *
+     * @returns
+     * This test case does not return any value. It expects the InvalidCensusDataException
+     * to be thrown during the execution of the loadCensusData method. If the method throws
+     * the expected exception, the test passes; otherwise, it fails.
+     *
+     * @throws IOException               If an I/O error occurs during the test.
+     * @throws CsvException              If an error occurs while processing the CSV file.
+     * @throws InvalidCensusDataException If the CSV file has correct data but incorrect types.
+     */
+    @Test(expected = InvalidCensusDataException.class)
+    public void testLoadCensusDataWithIncorrectType() throws IOException, CsvException, InvalidCensusDataException {
+        // Specify a correct CSV file path with incorrect types for testing
+        String correctCsvFilePath = "C:\\Users\\KIIT\\OneDrive\\Desktop\\StateCensus2.csv";
+
+        // Create CSVStateCensus instance
+        CSVStateCensus csvStateCensus = new CSVStateCensus();
+
+        // Attempt to load census data from the correct CSV file with incorrect types
+        csvStateCensus.loadCensusData(correctCsvFilePath);
+    }
 }

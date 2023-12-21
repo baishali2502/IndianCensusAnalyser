038157cf7c3a2e5f88168dce41bdc8fa8f3a7638
baishali2502 <baishali2502@gmail.com>
2023-12-21 14:42:25 +0530

    Added UC-1:Read from csv file

--- IndianCensusAnalyser/src/main/java/bridgelabz/com/indiancenususanalyser/CSVStateCensus.java
+++ IndianCensusAnalyser/src/main/java/bridgelabz/com/indiancenususanalyser/CSVStateCensus.java
@@ -11,6 +11,52 @@ import java.util.List;
 /**
  * A class responsible for reading Indian State Census data from a CSV file.
  */
-public class CSVStateCensus {
+public class CSVStateCensus 
+{
+	 /**
+     * @desc This method reads the state census data from a CSV file and converts it into a list of StateCensusData objects.
+     * It assumes that the values in the second column ("StateName") are already enclosed in double quotes.
+     * @params csvFilePath The path to the CSV file containing state census data.
+     * @throws IOException  If an I/O error occurs while reading the file.
+     * @throws CsvException If an error occurs while processing the CSV file.
+     * @returns A list of StateCensusData objects representing the loaded data.
+     */
+    public List<StateCensusData> loadCensusData(String csvFilePath) throws IOException, CsvException {
+        List<StateCensusData> censusDataList = new ArrayList<>();
+
+        try {
+            // Create a CSVReader for reading the CSV file
+            CSVReader csvReader = new CSVReader(new FileReader(csvFilePath));
+
+            // Read all data into a list of arrays
+            List<String[]> data = csvReader.readAll();
+
+            // Use an iterator to process each row
+            Iterator<String[]> iterator = data.iterator();
+            iterator.next(); // Skip header row
+
+            while (iterator.hasNext()) {
+                String[] row = iterator.next();
+
+                // Extract values from the row
+                int srNo = Integer.parseInt(row[0]);
+                String stateName = row[1];  // Assumes that the values in the second column are already enclosed in double quotes
+                int tin = Integer.parseInt(row[2]);
+                String stateCode = row[3];
+
+                // Create a StateCensusData object for each row and add it to the list
+                StateCensusData censusData = new StateCensusData(srNo, stateName, tin, stateCode);
+                censusDataList.add(censusData);
+            }
+
+            // Close the CSVReader
+            csvReader.close();
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+            throw e; // Rethrow the exception to the calling method
+        }
+
+        return censusDataList;
+    }
 
 }
--- IndianCensusAnalyser/src/main/java/bridgelabz/com/indiancenususanalyser/Main.java
+++ IndianCensusAnalyser/src/main/java/bridgelabz/com/indiancenususanalyser/Main.java
@@ -5,7 +5,8 @@ import java.util.List;
 import com.opencsv.exceptions.CsvException;
 public class Main 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println( " Welcome to Indian State Census Analyser !! " );
         
 	     // Specify the path to your CSV file
@@ -13,7 +14,26 @@ public class Main
 
 	        // Create CSVStateCensus instance
 	        CSVStateCensus csvStateCensus = new CSVStateCensus();
+	//--------------------------------------- UC-1 ----------------------------------------------
+	        // Load census data
+	        List<StateCensusData> censusDataList=null;
+			try {
+				censusDataList = csvStateCensus.loadCensusData(csvFilePath);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (CsvException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
-	
+	        // Check if the number of records matches
+	        int expectedRecordCount = 37;
+	        
+	        if (censusDataList.size() == expectedRecordCount) {
+	            System.out.println("Number of records match.");
+	        } else {
+	            System.out.println("Number of records do not match.");
+	        }	
 	}
 }

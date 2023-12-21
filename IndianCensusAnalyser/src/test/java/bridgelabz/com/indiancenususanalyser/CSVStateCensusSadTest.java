c711df87cdcb474e7c2b8e494506be4d4fef9a2f
baishali2502 <baishali2502@gmail.com>
2023-12-22 02:04:56 +0530

    Added TC-1.5:Throw exception for correct csv file but incorrect header

--- IndianCensusAnalyser/src/test/java/bridgelabz/com/indiancenususanalyser/CSVStateCensusSadTest.java
+++ IndianCensusAnalyser/src/test/java/bridgelabz/com/indiancenususanalyser/CSVStateCensusSadTest.java
@@ -106,4 +106,35 @@ public class CSVStateCensusSadTest {
         // Attempt to load census data from the correct CSV file with an incorrect delimiter
         csvStateCensus.loadCensusData(correctCsvFilePath);
     }
+    /**
+     * @desc
+     * This test case checks whether the CSVStateCensus class throws a custom exception
+     * (InvalidCensusDataException) when the CSV header is incorrect in a correct state census CSV file.
+     * This is a sad test case to ensure that an exception is raised in case of an incorrect header.
+     *
+     * @params
+     * The test uses the following parameters:
+     * - correctCsvFilePath: A string representing the path to the correct CSV file
+     *                       with an incorrect header used for testing.
+     *
+     * @returns
+     * This test case does not return any value. It expects the InvalidCensusDataException
+     * to be thrown during the execution of the loadCensusData method. If the method throws
+     * the expected exception, the test passes; otherwise, it fails.
+     *
+     * @throws IOException               If an I/O error occurs during the test.
+     * @throws CsvException              If an error occurs while processing the CSV file.
+     * @throws InvalidCensusDataException If the CSV file has correct data but an incorrect header.
+     */
+    @Test(expected = InvalidCensusDataException.class)
+    public void testLoadCensusDataWithIncorrectHeader() throws IOException, CsvException, InvalidCensusDataException {
+        // Specify a correct CSV file path with an incorrect header for testing
+        String correctCsvFilePath = "C:\\Users\\KIIT\\OneDrive\\Desktop\\StateCensus4.csv";
+
+        // Create CSVStateCensus instance
+        CSVStateCensus csvStateCensus = new CSVStateCensus();
+
+        // Attempt to load census data from the correct CSV file with an incorrect header
+        csvStateCensus.loadCensusData(correctCsvFilePath);
+    }
 }
